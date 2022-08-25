e4215333dd0b0d5d44d513b8c3fec9bea987e6b4
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2022-08-26 00:00:49 +0300

    Work with underscore map function.

---
+++ src/underscore_js/main.js
@@ -0,0 +1,15 @@
+const { _ , map} = require('underscore');
+
+let arr = [1, 2, 3];
+
+console.log(
+    _.map(arr, 
+        val => Math.imul(val, 2)
+    )
+);
+
+console.log(
+    _(arr).map( 
+        val => Math.imul(val, 2)
+    )
+);
